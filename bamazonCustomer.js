7c546e7b26f5245e53e5deaadb284e9cbb29944e
Khalid Madih <madih_khalid@hotmail.com>
2018-02-03 07:32:34 -0700

    Bamazon Customer App

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,7 +5,7 @@ var Table = require('cli-table');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'Barlabak1',
+  password : '',
   database : 'bamazon_db',
   port : '3306'
 });
